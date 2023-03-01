a7db478f8f2fed31d6b42e181afbd7066073629f
Oleg Aushev <aushevom@gmail.com>
2023-03-01 17:57:47 +0300

    update chrono

--- emb_chrono.h
+++ emb_chrono.h
@@ -21,8 +21,6 @@ public:
 	Duration() : _ticks(0) {}
 	explicit Duration(int64_t tick_count) : _ticks(tick_count) {}
 	Duration(const Duration& other) { this->_ticks = other._ticks; }
-//	Duration(const volatile Duration& other) { this->_ticks = other._ticks; }
-
 	Duration& operator=(const Duration& other)
 	{
 		if (this != &other)
@@ -31,15 +29,6 @@ public:
 		}
 		return *this;
 	}
-
-//	volatile Duration& operator=(const Duration& other) volatile
-//	{
-//		if (this != &other)
-//		{
-//			this->_ticks = other._ticks;
-//		}
-//		return *this;
-//	}
 
 	int64_t count() const { return _ticks; }
 
