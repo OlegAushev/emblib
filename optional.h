f9dad584485c24ab35145ada3036d281df74ac2d
Oleg Aushev <aushevom@gmail.com>
2023-10-30 14:47:10 +0300

    rework optional, add tests

--- optional.h
+++ optional.h
@@ -2,53 +2,65 @@
 
 
 #include <emblib/core.h>
+#include <new>
 
 
 namespace emb {
 
 
+struct nullopt_t {
+    explicit nullopt_t() {}
+};
+
+
+const nullopt_t nullopt;
+
+
 template <class T>
 class optional {
 private:
-    union {
-        char _dummy;
-        T _value;
-    };
+    char _storage[sizeof(T)];
     bool _has_value;
 public:
-    optional() : _dummy(0), _has_value(false) {}
-    optional(const T& value) : _value(value), _has_value(true) {}
+    optional() : _has_value(false) {}
+    optional(nullopt_t) : _has_value(false) {}
+    optional(const T& value) {
+        new(_storage) T(value);
+        _has_value = true;
+    }
+
     ~optional() {
         if (_has_value) {
-            _value.~T();
+            reinterpret_cast<T*>(_storage)->~T();
         }
     }
 
+    void reset() {
+        if (_has_value) {
+            _has_value = false;
+            reinterpret_cast<T*>(_storage)->~T();
+        }
+    }
+
+    bool has_value() const { return _has_value; }
+    const T& value() const { return *(reinterpret_cast<const T*>(_storage)); }
+    T value_or(const T& default_value) const { return _has_value ? value() : default_value; }
+
     optional& operator=(const T& value) {
-        _value = value;
+        reset();
+        new(_storage) T(value);
         _has_value = true;
         return *this;
     }
 
     optional& operator=(const optional<T> other) {
+        reset();
+        if (other._has_value) {
+            T value = other.value();
+            new(_storage) T(value);
+        }
         _has_value = other._has_value;
-        if (_has_value) {
-            _value = other._value;
-        } else {
-            _dummy = 0;
-        }
         return *this;
-    }
-
-    bool has_value() const { return _has_value; }
-    const T& value() const { return _value; }
-    T value_or(const T& default_value) { return _has_value ? _value : default_value; }
-
-    void reset() {
-        if (_has_value) {
-            _value.~T();
-            _has_value = false;
-        }
     }
 };
 
--- tests/optional_test.cpp
+++ tests/optional_test.cpp
@@ -1,6 +1,16 @@
 #include <emblib/tests/tests.h>
 
 
+struct Foo {
+    static int counter;
+    int id;
+    Foo() : id(++counter) {}
+    Foo(const Foo& other) : id(other.id) { ++counter; }
+    ~Foo() { --counter; }
+};
+int Foo::counter = 0;
+
+
 void emb::tests::optional_test() {
     emb::optional<int> opt1;
     EMB_ASSERT_TRUE(!opt1.has_value());
@@ -24,4 +34,24 @@ void emb::tests::optional_test() {
     opt3.reset();
     EMB_ASSERT_TRUE(!opt3.has_value());
     EMB_ASSERT_EQUAL(opt3.value_or(43), 43);
+
+    emb::optional<int> opt4 = emb::nullopt;
+    EMB_ASSERT_TRUE(!opt4.has_value());
+    opt4 = 44;
+    EMB_ASSERT_TRUE(opt4.has_value());
+    EMB_ASSERT_EQUAL(opt4.value(), 44);
+
+
+    emb::optional<Foo> fopt1 = Foo();
+    EMB_ASSERT_EQUAL(fopt1.value().id, 1);
+    EMB_ASSERT_EQUAL(Foo::counter, 1);
+
+    emb::optional<Foo> fopt2;
+    fopt2 = fopt1.value();
+    EMB_ASSERT_EQUAL(fopt2.value().id, 1);
+    EMB_ASSERT_EQUAL(Foo::counter, 2);
+
+    fopt2 = Foo();
+    EMB_ASSERT_EQUAL(fopt2.value().id, 3);
+    EMB_ASSERT_EQUAL(Foo::counter, 2);
 }
