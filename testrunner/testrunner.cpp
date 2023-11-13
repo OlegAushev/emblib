8129a62fbaf1e412197d12c0b6a8d789eea65c3e
Oleg Aushev <aushevom@gmail.com>
2023-11-13 14:02:16 +0300

    fix typo

--- testrunner/testrunner.cpp
+++ testrunner/testrunner.cpp
@@ -10,6 +10,9 @@ namespace emb {
 void (*test_runner::print)(const char* str) = test_runner::print_dbg;
 void (*test_runner::print_nextline)() = test_runner::print_nextline_dbg;
 
+#endif
+
+
 int test_runner::_asserts_in_test = 0;
 int test_runner::_asserts = 0;
 int test_runner::_asserts_failed_in_test = 0;
@@ -18,9 +21,6 @@ int test_runner::_tests = 0;
 int test_runner::_tests_failed = 0;
 int test_runner::_tests_passed = 0;
 int test_runner::_tests_skipped = 0;
-
-
-#endif
 
 
 }
