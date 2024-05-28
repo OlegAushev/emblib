de965fd9fe9fc3e0db92879215c4f40dd980b966
Oleg Aushev <aushevom@gmail.com>
2024-05-28 16:51:28 +0300

    add variant-fsm prototype

---
+++ fsm.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+#ifndef EMBLIB_C28X
+
+
+#include <optional>
+#include <variant>
+
+
+namespace emb {
+
+
+template<typename FsmType, typename... States>
+class fsm {
+private:
+    using state_variant_t = std::variant<States...>;
+    state_variant_t _state;
+public:
+    template<typename Event>
+    void dispatch(Event&& event) {
+        auto& child = static_cast<FsmType&>(*this);
+        const auto& visitor = [&](auto& state) -> std::optional<state_variant_t> {
+            return child.on_event(state, std::forward(event));
+        };
+        const auto& next_state = std::visit(visitor, _state);
+        if (next_state) {
+            _state = *std::move(next_state);
+        }
+    }
+};
+
+
+} // namespace fsm
+
+
+#endif
