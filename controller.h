388f964f01676d3b761cb4d14a08b56ca4938cea
Oleg Aushev <aushevom@gmail.com>
2024-09-27 17:01:55 +0300

    refactor pi_controller

--- controller.h
+++ controller.h
@@ -77,7 +77,7 @@ protected:
     float _kp;              // proportional gain
     float _ki;              // integral gain
     float _ts;              // sampling period
-    float _integrator_sum;  // integrator sum;
+    float _out_i;           // integrator sum;
     float _lower_limit;     // output lower limit
     float _upper_limit;     // output upper limit
     float _out;             // output;
@@ -88,7 +88,7 @@ public:
             : _kp(kp)
             , _ki(ki)
             , _ts(ts)
-            , _integrator_sum(0)
+            , _out_i(0)
             , _lower_limit(lower_limit)
             , _upper_limit(upper_limit)
             , _out(0) {}
@@ -96,7 +96,7 @@ public:
     virtual ~abstract_pi_controller() {}
     virtual void push(float ref, float meas) = 0;
     virtual void reset() {
-        _integrator_sum = 0;
+        _out_i = 0;
         _out = 0;
     }
     float output() const { return _out; }
@@ -109,7 +109,7 @@ public:
     void set_ki(float value) { _ki = value; }
     float kp() const { return _kp; }
     float ki() const { return _ki; }
-    float sum() const { return _integrator_sum; }
+    float integral() const { return _out_i; }
     void set_sampling_period(float value) { _ts = value; }
 };
 
@@ -136,10 +136,11 @@ public:
 
     virtual void push(float ref, float meas) EMB_OVERRIDE {
         float error = abstract_pi_controller<Logic>::_error(ref, meas);
-        float out = emb::clamp(error * this->_kp + this->_integrator_sum, -FLT_MAX, FLT_MAX);
+        float out = emb::clamp(error * this->_kp + this->_out_i, -FLT_MAX, FLT_MAX);
         this->_out = emb::clamp(out, this->_lower_limit, this->_upper_limit);
-        this->_integrator_sum = emb::clamp(this->_integrator_sum + this->_ki * this->_ts * error - _kc * (out - this->_out),
-                -FLT_MAX, FLT_MAX);
+        this->_out_i = emb::clamp(this->_out_i + this->_ki * this->_ts * error - _kc * (out - this->_out),
+                                  -FLT_MAX,
+                                  FLT_MAX);
     }
 };
 
@@ -159,29 +160,29 @@ public:
 
     virtual void push(float ref, float meas) EMB_OVERRIDE {
         float error = abstract_pi_controller<Logic>::_error(ref, meas);
-        float outp = error * this->_kp;
-        float sum_i = (error + _error) * 0.5f * this->_ki * this->_ts + this->_integrator_sum;
+        float out_p = error * this->_kp;
+        float out_i = (error + _error) * 0.5f * this->_ki * this->_ts + this->_out_i;
         _error = error;
-        float out = outp + sum_i;
+        float out = out_p + out_i;
 
         if (out > this->_upper_limit) {
             this->_out = this->_upper_limit;
-            if (outp < this->_upper_limit) {
-                this->_integrator_sum = this->_upper_limit - outp;
+            if (out_p < this->_upper_limit) {
+                this->_out_i = this->_upper_limit - out_p;
             }
         } else if (out < this->_lower_limit) {
             this->_out = this->_lower_limit;
-            if (outp > this->_lower_limit) {
-                this->_integrator_sum = this->_lower_limit - outp;
+            if (out_p > this->_lower_limit) {
+                this->_out_i = this->_lower_limit - out_p;
             }
         } else {
             this->_out = out;
-            this->_integrator_sum = sum_i;
+            this->_out_i = out_i;
         }
     }
 
     virtual void reset() EMB_OVERRIDE {
-        this->_integrator_sum = 0;
+        this->_out_i = 0;
         _error = 0;
         this->_out = 0;
     }
