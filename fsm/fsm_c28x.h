60c37a807dd413ae1fdecc89e0950d0f218da7d7
Oleg Aushev <aushevom@gmail.com>
2024-11-12 10:00:07 +0300

    c28x: update fsm - remove redundant

--- fsm/fsm_c28x.h
+++ fsm/fsm_c28x.h
@@ -1,28 +1,27 @@
 #pragma once
 
-
 #if defined(EMBLIB_C28X)
-
 
 #include <emblib/array.h>
 #include <emblib/chrono.h>
 
-
 namespace emb {
 namespace fsm {
 
-
-template<typename Object, typename State, typename AbstractState>
+template<typename Object, typename State>
 class abstract_state {
 private:
     const State _id;
     emb::chrono::milliseconds _enter_timepoint;
 protected:
-    abstract_state(State id) : _id(id), _enter_timepoint(emb::chrono::steady_clock::now()) {}
+    abstract_state(State id)
+            : _id(id),
+              _enter_timepoint(emb::chrono::steady_clock::now()) {}
     void change_state(Object* object, State state) {
         object->_current_state->_finalize(object);
         object->change_state(state);
-        object->_current_state->_enter_timepoint = emb::chrono::steady_clock::now();
+        object->_current_state->_enter_timepoint =
+            emb::chrono::steady_clock::now();
         object->_current_state->_initiate(object);
     }
     virtual void _initiate(Object* object) = 0;
@@ -30,9 +29,10 @@ protected:
 public:
     virtual ~abstract_state() {}
     State id() const { return _id; }
-    emb::chrono::milliseconds time_since_enter() const { return emb::chrono::steady_clock::now() - _enter_timepoint; }
+    emb::chrono::milliseconds time_since_enter() const {
+        return emb::chrono::steady_clock::now() - _enter_timepoint;
+    }
 };
-
 
 template<typename State, typename AbstractState, size_t StateCount>
 class abstract_object {
@@ -40,10 +40,10 @@ private:
     emb::array<AbstractState*, StateCount> _states;
 protected:
     AbstractState* _current_state;
-    State _current_stateid;
+    State _current_state_id;
     void change_state(State state) {
         _current_state = _states[state.underlying_value()];
-        _current_stateid = _current_state->id();
+        _current_state_id = _current_state->id();
     }
 public:
     abstract_object(State init_state) {
@@ -57,12 +57,10 @@ public:
             AbstractState::destroy(State(i), _states[i]);
         }
     }
-    State state() const { return _current_stateid; }
+    State state() const { return _current_state_id; }
 };
-
 
 } // namespace fsm
 } // namespace emb
-
 
 #endif
