a0abbb160dcab8bc8ad632b05c16be10ef876a8b
Oleg Aushev <aushevom@gmail.com>
2024-11-22 23:47:47 +0300

    move emb::gpio to mcu::gpio for c28x-arch

--- interfaces/gpio.h
+++ interfaces/gpio.h
@@ -1,48 +1,39 @@
 #pragma once
 
-
 #include "../core.h"
 
-
 namespace emb {
-
-
 namespace gpio {
-
 
 #if defined(EMBLIB_C28X)
 
-
-SCOPED_ENUM_DECLARE_BEGIN(active_state) {
-    low = 0,
-    high = 1
-} SCOPED_ENUM_DECLARE_END(active_state)
-
-
-SCOPED_ENUM_DECLARE_BEGIN(pin_state) {
-    inactive = 0,
-    active = 1
-} SCOPED_ENUM_DECLARE_END(pin_state)
-
+// OBSOLETE: moved to mcu::gpio
+//SCOPED_ENUM_DECLARE_BEGIN(active_state) {
+//    low = 0,
+//    high = 1
+//} SCOPED_ENUM_DECLARE_END(active_state)
+//
+//
+//SCOPED_ENUM_DECLARE_BEGIN(pin_state) {
+//    inactive = 0,
+//    active = 1
+//} SCOPED_ENUM_DECLARE_END(pin_state)
 
 #elif defined(EMBLIB_ARM)
-
 
 enum class active_state : unsigned int {
     low = 0,
     high = 1
 };
 
-
 enum class pin_state : unsigned int {
     inactive = 0,
     active = 1
 };
 
-
 #endif
 
-
+#if defined(EMBLIB_ARM)
 class input_pin {
 public:
     input_pin() EMB_DEFAULT
@@ -51,7 +42,6 @@ public:
     virtual pin_state read() const = 0;
     virtual unsigned int read_level() const = 0;
 };
-
 
 class output_pin {
 public:
@@ -65,9 +55,7 @@ public:
     virtual unsigned int read_level() const = 0;
     virtual void set_level(unsigned int level) = 0;
 };
-
+#endif
 
 } // namespace gpio
-
-
 } // namespace emb
