96ba3cd1e25a57b9bbcc78b3d7cee8128dd566a2
Oleg Aushev <aushevom@gmail.com>
2025-06-23 23:40:42 +0300

    split mcudef.hpp into gpio.hpp and uart.hpp

--- uart.hpp
+++ uart.hpp
@@ -5,81 +5,6 @@
 #include <emblib/scopedenum.hpp>
 
 namespace emb {
-namespace gpio {
-
-#if __cplusplus >= 201100
-
-enum class active_state : unsigned int {
-  low = 0,
-  high = 1
-};
-
-enum class state : unsigned int {
-  inactive = 0,
-  active = 1
-};
-
-class input {
-public:
-  input() = default;
-  virtual ~input() = default;
-
-  virtual state read() const = 0;
-  virtual unsigned int read_level() const = 0;
-};
-
-class output {
-public:
-  output() = default;
-  virtual ~output() = default;
-
-  virtual state read() const = 0;
-  virtual void set(state s = state::active) = 0;
-  virtual void reset() = 0;
-  virtual void toggle() = 0;
-  virtual unsigned int read_level() const = 0;
-  virtual void set_level(unsigned int level) = 0;
-};
-
-#else
-// clang-format off
-SCOPED_ENUM_UT_DECLARE_BEGIN(active_state, unsigned int) {
-  low = 0,
-  high = 1
-} SCOPED_ENUM_DECLARE_END(active_state);
-
-SCOPED_ENUM_UT_DECLARE_BEGIN(state, unsigned int) {
-  inactive = 0,
-  active = 1
-} SCOPED_ENUM_DECLARE_END(state);
-// clang-format on
-
-class input {
-public:
-  input() {}
-  virtual ~input() {}
-
-  virtual state read() const = 0;
-  virtual unsigned int read_level() const = 0;
-};
-
-class output {
-public:
-  output() {}
-  virtual ~output() {}
-
-  virtual state read() const = 0;
-  virtual void set(state s = state::active) = 0;
-  virtual void reset() = 0;
-  virtual void toggle() = 0;
-  virtual unsigned int read_level() const = 0;
-  virtual void set_level(unsigned int level) = 0;
-};
-
-#endif
-
-} // namespace gpio
-
 namespace uart {
 
 class tty {
@@ -138,5 +63,4 @@ public:
 #endif
 
 } // namespace uart
-
 } // namespace emb
