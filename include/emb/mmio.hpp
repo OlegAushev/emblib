05564fe12c9b3216f86478961ab933f0b116722d
Oleg Aushev <aushevom@gmail.com>
2026-01-28 01:01:16 +0300

    add new mmio implementation

--- include/emb/mmio.hpp
+++ include/emb/mmio.hpp
@@ -2,116 +2,111 @@
 
 #ifdef __cpp_concepts
 
-#include <emb/core.hpp>
+#include <emb/meta.hpp>
 
 #include <bit>
 #include <concepts>
+#include <cstdint>
+#include <limits>
 
 namespace emb {
 namespace mmio {
-namespace cmsis {
 
-// Concept: Peripheral must have regs_type and regs.
-template<typename Peripheral>
-concept peripheral = requires {
-  typename Peripheral::regs_type;
-  { *Peripheral::regs } -> std::same_as<typename Peripheral::regs_type&>;
-};
+template<typename T>
+concept reg_address =
+    emb::same_as_any<T, uint8_t, uint16_t, uint32_t, uint64_t, uintptr_t>;
 
-// Concept: Register must be valid member pointer for Peripheral::regs_type.
-template<typename Peripheral, auto Reg>
-concept valid_reg = peripheral<Peripheral> &&
-                    requires(Peripheral::regs_type* regs) {
-                      { regs->*Reg };
-                    };
+template<typename T>
+concept reg_width = emb::same_as_any<T, uint8_t, uint16_t, uint32_t, uint64_t>;
 
-// Helper type (needs valid_reg to be safe).
-template<peripheral Peripheral, auto Reg>
-  requires valid_reg<Peripheral, Reg>
-using reg_type = std::remove_cvref_t<decltype(Peripheral::regs->*Reg)>;
+template<auto Address, reg_width Width = uint32_t>
+  requires reg_address<decltype(Address)>
+struct reg {
+  using value_type = Width;
 
-// Concept: Mask must be convertible to register type.
-template<typename Peripheral, auto Reg, auto Mask>
-concept valid_mask =
-    valid_reg<Peripheral, Reg> &&
-    std::convertible_to<decltype(Mask), reg_type<Peripheral, Reg>>;
-
-template<peripheral Peripheral, auto Reg, auto Mask>
-  requires valid_mask<Peripheral, Reg, Mask>
-[[nodiscard]] auto read() {
-  using T = reg_type<Peripheral, Reg>;
-  constexpr auto mask = static_cast<T>(Mask);
-  constexpr auto offset = std::countr_zero(mask);
-  auto& reg = Peripheral::regs->*Reg;
-  return (reg & mask) >> offset;
-}
-template<peripheral Peripheral, auto Reg, auto Mask>
-  requires valid_mask<Peripheral, Reg, Mask>
-void write(reg_type<Peripheral, Reg> val) {
-  using T = reg_type<Peripheral, Reg>;
-  constexpr auto mask = static_cast<T>(Mask);
-  constexpr auto offset = std::countr_zero(mask);
-  auto& reg = Peripheral::regs->*Reg;
-  reg = (reg & ~mask) | ((val << offset) & mask);
-}
-
-template<peripheral Peripheral, auto Reg>
-  requires valid_reg<Peripheral, Reg>
-[[nodiscard]] auto read() {
-  return Peripheral::regs->*Reg;
-}
-
-template<peripheral Peripheral, auto Reg>
-  requires valid_reg<Peripheral, Reg>
-void write(reg_type<Peripheral, Reg> val) {
-  Peripheral::regs->*Reg = val;
-}
-
-template<typename Policy, peripheral Peripheral, auto Reg, auto Mask>
-  requires valid_mask<Peripheral, Reg, Mask>
-void set() {
-  using T = reg_type<Peripheral, Reg>;
-  Policy::set(Peripheral::regs->*Reg, static_cast<T>(Mask));
-}
-
-template<typename Policy, peripheral Peripheral, auto Reg, auto Mask>
-  requires valid_mask<Peripheral, Reg, Mask>
-void clear() {
-  using T = reg_type<Peripheral, Reg>;
-  Policy::clear(Peripheral::regs->*Reg, static_cast<T>(Mask));
-}
-
-// Policies define how to set/clear bits.
-struct rw {
-  template<typename Reg, typename Mask>
-  static void set(Reg& reg, Mask mask) {
-    reg |= mask;
+  static value_type read() {
+    return *reinterpret_cast<value_type const volatile*>(Address);
   }
 
-  template<typename Reg, typename Mask>
-  static void clear(Reg& reg, Mask mask) {
-    reg &= ~mask;
+  static void write(value_type value) {
+    *reinterpret_cast<value_type volatile*>(Address) = value;
   }
 };
 
-struct rc_w0 {
-  template<typename Reg, typename Mask>
-  static void clear(Reg& reg, Mask mask) {
-    reg = ~mask;
+struct ro {
+  template<typename T>
+  static T read(T const volatile* reg, T mask, unsigned offset) {
+    return (*reg & mask) >> offset;
   }
 };
 
-struct rc_w1 {
-  template<typename Reg, typename Mask>
-  static void clear(Reg& reg, Mask mask) {
-    reg = mask;
+struct rw : public ro {
+  template<typename T>
+  static void write(T volatile* reg, T mask, unsigned offset, T value) {
+    *reg = (*reg & ~mask) | ((value << offset) & mask);
+  }
+
+  template<typename T>
+  static void set(T volatile* reg, T mask) {
+    *reg = *reg | mask;
+  }
+
+  template<typename T>
+  static void clear(T volatile* reg, T mask) {
+    *reg = *reg & ~mask;
   }
 };
 
-} // namespace cmsis
+struct rc_w0 : public ro {
+  template<typename T>
+  static void clear(T volatile* reg, T mask) {
+    *reg = ~mask;
+  }
+};
+
+struct rc_w1 : public ro {
+  template<typename T>
+  static void clear(T volatile* reg, T mask) {
+    *reg = mask;
+  }
+};
+
+template<auto Address, auto Mask, typename Policy = rw>
+  requires reg_address<decltype(Address)> &&
+           reg_width<decltype(Mask)> &&
+           (Mask != 0)
+struct field {
+  using value_type = decltype(Mask);
+  using pointer = value_type volatile*;
+  using const_pointer = value_type const volatile*;
+  using policy_type = Policy;
+
+  static constexpr auto address = Address;
+  static constexpr value_type mask = Mask;
+  static constexpr unsigned offset = std::countr_zero(mask);
+
+  static value_type read() {
+    return policy_type::read(
+        reinterpret_cast<const_pointer>(address),
+        mask,
+        offset
+    );
+  }
+
+  static void write(value_type value) {
+    policy_type::write(reinterpret_cast<pointer>(address), mask, offset, value);
+  }
+
+  static void set() {
+    policy_type::set(reinterpret_cast<pointer>(address), mask);
+  }
+
+  static void clear() {
+    policy_type::clear(reinterpret_cast<pointer>(address), mask);
+  }
+};
+
 } // namespace mmio
 } // namespace emb
-
-#define EMB_MMIO_CMSIS_REG(name) static constexpr auto name = &regs_type::name
 
 #endif
--- tests/mmio_test.cpp
+++ tests/mmio_test.cpp
@@ -8,61 +8,14 @@ namespace emb {
 namespace internal {
 namespace tests {
 
-// struct tim {
-//   using regs_type = TMR_TypeDef;
-//   static inline regs_type* const regs = TMR1;
-//   EMB_MMIO_CMSIS_REG(CTRL1);
-// };
-
-// static_assert(mmio::cmsis::peripheral<tim>);
-
-// inline void test() {
-//   mmio::cmsis::write<tim, tim::CTRL1, TMR_CTRL1_CNTEN>(1);
-// }
-
-struct mock_regs {
-  uint32_t ctrl;
-  uint32_t status;
-  uint8_t data;
-};
-
-struct mock_peripheral {
-  using regs_type = mock_regs;
-  static regs_type* regs;
-  static constexpr void init(mock_regs& r) {
-    regs = &r;
-  }
-  EMB_MMIO_CMSIS_REG(ctrl);
-  EMB_MMIO_CMSIS_REG(status);
-  EMB_MMIO_CMSIS_REG(data);
-};
-
-static_assert(mmio::cmsis::peripheral<mock_peripheral>);
-
 constexpr bool test_mmio() {
-
-
-
-
-
-  // mock_regs r;
-  // mock_peripheral::init(r);
-
-  // mmio::cmsis::write<mock_peripheral, mock_peripheral::ctrl, 0x00000FF0>(42);
-  // [[maybe_unused]] auto val = mmio::cmsis::read<tim, tim::ctrl, 0x10>(1);
-  // assert(val == 42);
-
-  // uint32_t reg32 = 0;
-  // mmio::write()  _reg<mmio::rw>(reg32, 0x12345678u);
-  // assert(mmio::read_reg<mmio::rw>(reg32) == 0x12345678);
-
   return true;
 }
 
 static_assert(test_mmio());
 
-}
-}
-}
+} // namespace tests
+} // namespace internal
+} // namespace emb
 
 #endif
