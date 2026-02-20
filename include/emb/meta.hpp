0139d02e1311cf892fe82c0aa1890139556a2cd2
Oleg Aushev <aushevom@gmail.com>
2026-02-20 12:52:05 +0300

    add overload utility for type-safe visitation

--- include/emb/meta.hpp
+++ include/emb/meta.hpp
@@ -11,4 +11,13 @@ namespace emb {
 template<typename T, typename... Ts>
 concept same_as_any = (... || std::same_as<T, Ts>);
 
+template<typename... Ts>
+struct overload : Ts... {
+  using Ts::operator()...;
+
+  consteval void operator()(auto) const {
+    static_assert(false, "Unsupported type");
+  }
+};
+
 } // namespace emb
