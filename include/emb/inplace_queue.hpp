8cf307864de75992a8b9e68d631fa0ada4e7dd33
Oleg Aushev <aushevom@gmail.com>
2026-04-23 15:38:34 +0300

    inplace_stack, inplace_queue: unify specializations via union slots
    Replace the two partial specializations (trivial / non-trivial) with a
    single class using union-based slot storage and C++20
    requires-conditional defaulted special members, preserving triviality
    for trivially copyable T. Extract destroy_slot / copy_construct_from /
    move_construct_from helpers. Drop is_default_constructible_v<T>
    requirement. For non-trivial T, move eagerly clears source.

--- include/emb/inplace_queue.hpp
+++ include/emb/inplace_queue.hpp
@@ -11,134 +11,8 @@
 namespace emb {
 
 template<typename T, std::size_t Capacity>
-class inplace_queue;
-
-template<typename T, size_t Capacity>
-  requires std::is_default_constructible_v<T>
-        && std::is_trivially_destructible_v<T>
-        && (Capacity > 0)
-class inplace_queue<T, Capacity> {
-public:
-  using value_type = T;
-  using size_type = std::size_t;
-  using reference = value_type&;
-  using const_reference = value_type const&;
-  using pointer = value_type*;
-  using const_pointer = value_type const*;
-
-private:
-  std::array<value_type, Capacity> data_{};
-  static constexpr size_type capacity_ = Capacity;
-  size_type front_ = 0;
-  size_type size_ = 0;
-
-  constexpr size_type index_of(size_type offset) const {
-    return (front_ + offset) % capacity_;
-  }
-public:
-  constexpr inplace_queue() = default;
-  constexpr inplace_queue(inplace_queue const&) = default;
-  constexpr inplace_queue(inplace_queue&&) = default;
-  constexpr inplace_queue& operator=(inplace_queue const&) = default;
-  constexpr inplace_queue& operator=(inplace_queue&&) = default;
-  constexpr ~inplace_queue() = default;
-
-  constexpr void clear() {
-    front_ = 0;
-    size_ = 0;
-  }
-
-  [[nodiscard]] constexpr bool empty() const {
-    return size_ == 0;
-  }
-
-  [[nodiscard]] constexpr bool full() const {
-    return size_ == capacity_;
-  }
-
-  [[nodiscard]] constexpr size_type capacity() const {
-    return capacity_;
-  }
-
-  [[nodiscard]] constexpr size_type size() const {
-    return size_;
-  }
-
-  [[nodiscard]] constexpr reference front() {
-    ASSUME(!empty());
-    return data_[front_];
-  }
-
-  [[nodiscard]] constexpr const_reference front() const {
-    ASSUME(!empty());
-    return data_[front_];
-  }
-
-  [[nodiscard]] constexpr reference back() {
-    ASSUME(!empty());
-    return data_[index_of(size_ - 1)];
-  }
-
-  [[nodiscard]] constexpr const_reference back() const {
-    ASSUME(!empty());
-    return data_[index_of(size_ - 1)];
-  }
-
-  constexpr void push(value_type const& value)
-    requires std::is_copy_assignable_v<value_type> {
-    ASSUME(!full());
-    data_[index_of(size_)] = value;
-    ++size_;
-  }
-
-  constexpr void push(value_type&& value)
-    requires std::is_move_assignable_v<value_type> {
-    ASSUME(!full());
-    data_[index_of(size_)] = std::move(value);
-    ++size_;
-  }
-
-  template<typename... Args>
-  constexpr void emplace(Args&&... args)
-    requires std::is_constructible_v<value_type, Args...> {
-    ASSUME(!full());
-    std::construct_at(&data_[index_of(size_)], std::forward<Args>(args)...);
-    ++size_;
-  }
-
-  [[nodiscard]] constexpr bool try_push(value_type const& value)
-    requires std::is_copy_assignable_v<value_type> {
-    if (full()) return false;
-    push(value);
-    return true;
-  }
-
-  [[nodiscard]] constexpr bool try_push(value_type&& value)
-    requires std::is_move_assignable_v<value_type> {
-    if (full()) return false;
-    push(std::move(value));
-    return true;
-  }
-
-  constexpr void pop() {
-    ASSUME(!empty());
-    front_ = (front_ + 1) % capacity_;
-    --size_;
-  }
-
-  [[nodiscard]] constexpr std::optional<value_type> try_pop() {
-    if (empty()) return std::nullopt;
-    std::optional<value_type> result{front()};
-    pop();
-    return result;
-  }
-};
-
-template<typename T, std::size_t Capacity>
-  requires(!std::is_default_constructible_v<T>
-           || !std::is_trivially_destructible_v<T>)
-       && (Capacity > 0)
-class inplace_queue<T, Capacity> {
+  requires(Capacity > 0)
+class inplace_queue {
 public:
   using value_type = T;
   using size_type = std::size_t;
@@ -153,7 +27,15 @@ private:
     no_value_t no_value;
     value_type value;
     constexpr slot() : no_value{} {}
-    constexpr ~slot() {}
+    constexpr slot(slot const&) = default;
+    constexpr slot(slot&&) = default;
+    constexpr slot& operator=(slot const&) = default;
+    constexpr slot& operator=(slot&&) = default;
+    constexpr ~slot()
+      requires std::is_trivially_destructible_v<T>
+    = default;
+    constexpr ~slot()
+      requires(!std::is_trivially_destructible_v<T>) {}
   };
 
   std::array<slot, Capacity> data_{};
@@ -161,80 +43,77 @@ private:
   size_type front_ = 0;
   size_type size_ = 0;
 
-  constexpr size_type index_of(size_type offset) const {
-    return (front_ + offset) % capacity_;
-  }
-
-  constexpr pointer slot_ptr(size_type i) {
-    return &data_[i].value;
-  }
-
-  constexpr const_pointer slot_ptr(size_type i) const {
-    return &data_[i].value;
-  }
-
 public:
   constexpr inplace_queue() = default;
 
+  constexpr inplace_queue(inplace_queue const&)
+    requires std::is_trivially_copyable_v<T>
+  = default;
+
   constexpr inplace_queue(inplace_queue const& other)
-    requires std::is_copy_constructible_v<T> {
-    for (size_type i = 0; i < other.size_; ++i) {
-      std::construct_at(slot_ptr(i), *other.slot_ptr(other.index_of(i)));
-      ++size_;
-    }
+    requires(
+        std::is_copy_constructible_v<T> && !std::is_trivially_copyable_v<T>
+    ) {
+    copy_construct_from(other);
   }
+
+  constexpr inplace_queue(inplace_queue&&)
+    requires std::is_trivially_copyable_v<T>
+  = default;
 
   constexpr inplace_queue(
       inplace_queue&& other
   ) noexcept(std::is_nothrow_move_constructible_v<T>)
-    requires std::is_move_constructible_v<T> {
-    for (size_type i = 0; i < other.size_; ++i) {
-      std::construct_at(
-          slot_ptr(i),
-          std::move(*other.slot_ptr(other.index_of(i)))
-      );
-      ++size_;
-    }
-    other.clear();
+    requires(
+        std::is_move_constructible_v<T> && !std::is_trivially_copyable_v<T>
+    ) {
+    move_construct_from(other);
   }
 
+  constexpr inplace_queue& operator=(inplace_queue const&)
+    requires std::is_trivially_copyable_v<T>
+  = default;
+
   constexpr inplace_queue& operator=(inplace_queue const& other)
-    requires std::is_copy_constructible_v<T> {
+    requires(
+        std::is_copy_constructible_v<T> && !std::is_trivially_copyable_v<T>
+    ) {
     if (this != &other) {
       clear();
-      for (size_type i = 0; i < other.size_; ++i) {
-        std::construct_at(slot_ptr(i), *other.slot_ptr(other.index_of(i)));
-        ++size_;
-      }
+      copy_construct_from(other);
     }
     return *this;
   }
+
+  constexpr inplace_queue& operator=(inplace_queue&&)
+    requires std::is_trivially_copyable_v<T>
+  = default;
 
   constexpr inplace_queue& operator=(
       inplace_queue&& other
   ) noexcept(std::is_nothrow_move_constructible_v<T>)
-    requires std::is_move_constructible_v<T> {
+    requires(
+        std::is_move_constructible_v<T> && !std::is_trivially_copyable_v<T>
+    ) {
     if (this != &other) {
       clear();
-      for (size_type i = 0; i < other.size_; ++i) {
-        std::construct_at(
-            slot_ptr(i),
-            std::move(*other.slot_ptr(other.index_of(i)))
-        );
-        ++size_;
-      }
-      other.clear();
+      move_construct_from(other);
     }
     return *this;
   }
 
-  constexpr ~inplace_queue() {
+  constexpr ~inplace_queue()
+    requires std::is_trivially_destructible_v<T>
+  = default;
+
+  constexpr ~inplace_queue()
+    requires(!std::is_trivially_destructible_v<T>) {
     clear();
   }
 
   constexpr void clear() {
     for (size_type i = 0; i < size_; ++i) {
-      std::destroy_at(slot_ptr(index_of(i)));
+      destroy_slot(index_of(i));
     }
     front_ = 0;
     size_ = 0;
@@ -314,7 +193,7 @@ public:
 
   constexpr void pop() {
     ASSUME(!empty());
-    std::destroy_at(slot_ptr(front_));
+    destroy_slot(front_);
     front_ = (front_ + 1) % capacity_;
     --size_;
   }
@@ -332,6 +211,45 @@ public:
       pop();
       return result;
     }
+  }
+
+private:
+  constexpr size_type index_of(size_type offset) const {
+    return (front_ + offset) % capacity_;
+  }
+
+  constexpr pointer slot_ptr(size_type i) {
+    return &data_[i].value;
+  }
+
+  constexpr const_pointer slot_ptr(size_type i) const {
+    return &data_[i].value;
+  }
+
+  constexpr void destroy_slot(size_type i) {
+    if constexpr (!std::is_trivially_destructible_v<T>) {
+      std::destroy_at(slot_ptr(i));
+    }
+  }
+
+  constexpr void copy_construct_from(inplace_queue const& other) {
+    for (size_type i = 0; i < other.size_; ++i) {
+      std::construct_at(slot_ptr(i), *other.slot_ptr(other.index_of(i)));
+      ++size_;
+    }
+  }
+
+  constexpr void move_construct_from(
+      inplace_queue& other
+  ) noexcept(std::is_nothrow_move_constructible_v<T>) {
+    for (size_type i = 0; i < other.size_; ++i) {
+      std::construct_at(
+          slot_ptr(i),
+          std::move(*other.slot_ptr(other.index_of(i)))
+      );
+      ++size_;
+    }
+    other.clear();
   }
 };
 
--- include/emb/inplace_stack.hpp
+++ include/emb/inplace_stack.hpp
@@ -6,122 +6,13 @@
 #include <memory>
 #include <optional>
 #include <type_traits>
+#include <utility>
 
 namespace emb {
 
 template<typename T, std::size_t Capacity>
-class inplace_stack;
-
-template<typename T, std::size_t Capacity>
-  requires std::is_default_constructible_v<T>
-        && std::is_trivially_destructible_v<T>
-        && (Capacity > 0)
-class inplace_stack<T, Capacity> {
-public:
-  using value_type = T;
-  using size_type = std::size_t;
-  using reference = value_type&;
-  using const_reference = value_type const&;
-  using pointer = value_type*;
-  using const_pointer = value_type const*;
-
-private:
-  std::array<value_type, Capacity> data_{};
-  static constexpr size_type capacity_ = Capacity;
-  size_type size_ = 0;
-
-public:
-  constexpr inplace_stack() = default;
-  constexpr inplace_stack(inplace_stack const&) = default;
-  constexpr inplace_stack(inplace_stack&&) = default;
-  constexpr inplace_stack& operator=(inplace_stack const&) = default;
-  constexpr inplace_stack& operator=(inplace_stack&&) = default;
-  constexpr ~inplace_stack() = default;
-
-  constexpr void clear() {
-    size_ = 0;
-  }
-
-  [[nodiscard]] constexpr bool empty() const {
-    return size_ == 0;
-  }
-
-  [[nodiscard]] constexpr bool full() const {
-    return size_ == capacity_;
-  }
-
-  [[nodiscard]] constexpr size_type capacity() const {
-    return capacity_;
-  }
-
-  [[nodiscard]] constexpr size_type size() const {
-    return size_;
-  }
-
-  [[nodiscard]] constexpr reference top() {
-    ASSUME(!empty());
-    return data_[size_ - 1];
-  }
-
-  [[nodiscard]] constexpr const_reference top() const {
-    ASSUME(!empty());
-    return data_[size_ - 1];
-  }
-
-  constexpr void push(value_type const& value)
-    requires std::is_copy_assignable_v<value_type> {
-    ASSUME(!full());
-    data_[size_] = value;
-    ++size_;
-  }
-
-  constexpr void push(value_type&& value)
-    requires std::is_move_assignable_v<value_type> {
-    ASSUME(!full());
-    data_[size_] = std::move(value);
-    ++size_;
-  }
-
-  template<typename... Args>
-  constexpr void emplace(Args&&... args)
-    requires std::is_constructible_v<value_type, Args...> {
-    ASSUME(!full());
-    std::construct_at(&data_[size_], std::forward<Args>(args)...);
-    ++size_;
-  }
-
-  [[nodiscard]] constexpr bool try_push(value_type const& value)
-    requires std::is_copy_assignable_v<value_type> {
-    if (full()) return false;
-    push(value);
-    return true;
-  }
-
-  [[nodiscard]] constexpr bool try_push(value_type&& value)
-    requires std::is_move_assignable_v<value_type> {
-    if (full()) return false;
-    push(std::move(value));
-    return true;
-  }
-
-  constexpr void pop() {
-    ASSUME(!empty());
-    --size_;
-  }
-
-  [[nodiscard]] constexpr std::optional<value_type> try_pop() {
-    if (empty()) return std::nullopt;
-    std::optional<value_type> result{top()};
-    pop();
-    return result;
-  }
-};
-
-template<typename T, std::size_t Capacity>
-  requires(!std::is_default_constructible_v<T>
-           || !std::is_trivially_destructible_v<T>)
-       && (Capacity > 0)
-class inplace_stack<T, Capacity> {
+  requires(Capacity > 0)
+class inplace_stack {
 public:
   using value_type = T;
   using size_type = std::size_t;
@@ -136,77 +27,92 @@ private:
     no_value_t no_value;
     value_type value;
     constexpr slot() : no_value{} {}
-    constexpr ~slot() {}
+    constexpr slot(slot const&) = default;
+    constexpr slot(slot&&) = default;
+    constexpr slot& operator=(slot const&) = default;
+    constexpr slot& operator=(slot&&) = default;
+    constexpr ~slot()
+      requires std::is_trivially_destructible_v<T>
+    = default;
+    constexpr ~slot()
+      requires(!std::is_trivially_destructible_v<T>) {}
   };
 
   std::array<slot, Capacity> data_{};
   static constexpr size_type capacity_ = Capacity;
   size_type size_ = 0;
 
-  constexpr pointer slot_ptr(size_type i) {
-    return &data_[i].value;
-  }
-
-  constexpr const_pointer slot_ptr(size_type i) const {
-    return &data_[i].value;
-  }
-
 public:
   constexpr inplace_stack() = default;
 
+  constexpr inplace_stack(inplace_stack const&)
+    requires std::is_trivially_copyable_v<T>
+  = default;
+
   constexpr inplace_stack(inplace_stack const& other)
-    requires std::is_copy_constructible_v<T> {
-    for (size_type i = 0; i < other.size_; ++i) {
-      std::construct_at(slot_ptr(i), *other.slot_ptr(i));
-      ++size_;
-    }
+    requires(
+        std::is_copy_constructible_v<T> && !std::is_trivially_copyable_v<T>
+    ) {
+    copy_construct_from(other);
   }
+
+  constexpr inplace_stack(inplace_stack&&)
+    requires std::is_trivially_copyable_v<T>
+  = default;
 
   constexpr inplace_stack(
       inplace_stack&& other
   ) noexcept(std::is_nothrow_move_constructible_v<T>)
-    requires std::is_move_constructible_v<T> {
-    for (size_type i = 0; i < other.size_; ++i) {
-      std::construct_at(slot_ptr(i), std::move(*other.slot_ptr(i)));
-      ++size_;
-    }
-    other.clear();
+    requires(
+        std::is_move_constructible_v<T> && !std::is_trivially_copyable_v<T>
+    ) {
+    move_construct_from(other);
   }
 
+  constexpr inplace_stack& operator=(inplace_stack const&)
+    requires std::is_trivially_copyable_v<T>
+  = default;
+
   constexpr inplace_stack& operator=(inplace_stack const& other)
-    requires std::is_copy_constructible_v<T> {
+    requires(
+        std::is_copy_constructible_v<T> && !std::is_trivially_copyable_v<T>
+    ) {
     if (this != &other) {
       clear();
-      for (size_type i = 0; i < other.size_; ++i) {
-        std::construct_at(slot_ptr(i), *other.slot_ptr(i));
-        ++size_;
-      }
+      copy_construct_from(other);
     }
     return *this;
   }
+
+  constexpr inplace_stack& operator=(inplace_stack&&)
+    requires std::is_trivially_copyable_v<T>
+  = default;
 
   constexpr inplace_stack& operator=(
       inplace_stack&& other
   ) noexcept(std::is_nothrow_move_constructible_v<T>)
-    requires std::is_move_constructible_v<T> {
+    requires(
+        std::is_move_constructible_v<T> && !std::is_trivially_copyable_v<T>
+    ) {
     if (this != &other) {
       clear();
-      for (size_type i = 0; i < other.size_; ++i) {
-        std::construct_at(slot_ptr(i), std::move(*other.slot_ptr(i)));
-        ++size_;
-      }
-      other.clear();
+      move_construct_from(other);
     }
     return *this;
   }
 
-  constexpr ~inplace_stack() {
+  constexpr ~inplace_stack()
+    requires std::is_trivially_destructible_v<T>
+  = default;
+
+  constexpr ~inplace_stack()
+    requires(!std::is_trivially_destructible_v<T>) {
     clear();
   }
 
   constexpr void clear() {
     for (size_type i = size_; i > 0; --i) {
-      std::destroy_at(slot_ptr(i - 1));
+      destroy_slot(i - 1);
     }
     size_ = 0;
   }
@@ -275,7 +181,7 @@ public:
 
   constexpr void pop() {
     ASSUME(!empty());
-    std::destroy_at(slot_ptr(size_ - 1));
+    destroy_slot(size_ - 1);
     --size_;
   }
 
@@ -292,6 +198,38 @@ public:
       pop();
       return result;
     }
+  }
+
+private:
+  constexpr pointer slot_ptr(size_type i) {
+    return &data_[i].value;
+  }
+
+  constexpr const_pointer slot_ptr(size_type i) const {
+    return &data_[i].value;
+  }
+
+  constexpr void destroy_slot(size_type i) {
+    if constexpr (!std::is_trivially_destructible_v<T>) {
+      std::destroy_at(slot_ptr(i));
+    }
+  }
+
+  constexpr void copy_construct_from(inplace_stack const& other) {
+    for (size_type i = 0; i < other.size_; ++i) {
+      std::construct_at(slot_ptr(i), *other.slot_ptr(i));
+      ++size_;
+    }
+  }
+
+  constexpr void move_construct_from(
+      inplace_stack& other
+  ) noexcept(std::is_nothrow_move_constructible_v<T>) {
+    for (size_type i = 0; i < other.size_; ++i) {
+      std::construct_at(slot_ptr(i), std::move(*other.slot_ptr(i)));
+      ++size_;
+    }
+    other.clear();
   }
 };
 
