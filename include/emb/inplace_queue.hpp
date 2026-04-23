4d283fd908e7a8af7c9a4eaa10616cce8dfdae73
Oleg Aushev <aushevom@gmail.com>
2026-04-23 11:58:10 +0300

    inplace_queue: support non-default-constructible and non-trivially-destructible types
    Split inplace_queue into two partial specializations:
    - trivial case (default-constructible + trivially-destructible T) keeps
      the simple std::array storage and assignment-based push
    - general case stores a std::array of unions and manages element lifetime
      explicitly via std::construct_at / std::destroy_at
    
    Also add push(T&&), emplace, try_push(T&&), and tests covering lifetime,
    copy, and move semantics via a ref-counted tracked type.

--- include/emb/inplace_queue.hpp
+++ include/emb/inplace_queue.hpp
@@ -1,18 +1,22 @@
 #pragma once
 
-#include <emb/core.hpp>
-
 #include <array>
+#include <cassert>
+#include <memory>
 #include <optional>
 #include <type_traits>
+#include <utility>
 
 namespace emb {
+
+template<typename T, size_t Capacity>
+class inplace_queue;
 
 template<typename T, size_t Capacity>
   requires std::is_default_constructible_v<T>
         && std::is_trivially_destructible_v<T>
         && (Capacity > 0)
-class inplace_queue {
+class inplace_queue<T, Capacity> {
 public:
   using value_type = T;
   using size_type = size_t;
@@ -20,14 +24,23 @@ public:
   using const_reference = value_type const&;
   using pointer = value_type*;
   using const_pointer = value_type const*;
-  using underlying_type = std::array<value_type, Capacity>;
+
 private:
-  underlying_type data_{};
+  std::array<value_type, Capacity> data_{};
   static constexpr size_type capacity_ = Capacity;
   size_type front_ = 0;
   size_type size_ = 0;
+
+  constexpr size_type index_of(size_type offset) const {
+    return (front_ + offset) % capacity_;
+  }
 public:
   constexpr inplace_queue() = default;
+  constexpr inplace_queue(inplace_queue const&) = default;
+  constexpr inplace_queue(inplace_queue&&) = default;
+  constexpr inplace_queue& operator=(inplace_queue const&) = default;
+  constexpr inplace_queue& operator=(inplace_queue&&) = default;
+  constexpr ~inplace_queue() = default;
 
   constexpr void clear() {
     front_ = 0;
@@ -50,18 +63,6 @@ public:
     return size_;
   }
 
-  constexpr void push(value_type const& value) {
-    assert(!full());
-    data_[(front_ + size_) % capacity_] = value;
-    ++size_;
-  }
-
-  [[nodiscard]] constexpr bool try_push(value_type const& value) {
-    if (full()) return false;
-    push(value);
-    return true;
-  }
-
   [[nodiscard]] constexpr const_reference front() const {
     assert(!empty());
     return data_[front_];
@@ -69,7 +70,43 @@ public:
 
   [[nodiscard]] constexpr const_reference back() const {
     assert(!empty());
-    return data_[(front_ + size_ - 1) % capacity_];
+    return data_[index_of(size_ - 1)];
+  }
+
+  constexpr void push(value_type const& value)
+    requires std::is_copy_assignable_v<value_type> {
+    assert(!full());
+    data_[index_of(size_)] = value;
+    ++size_;
+  }
+
+  constexpr void push(value_type&& value)
+    requires std::is_move_assignable_v<value_type> {
+    assert(!full());
+    data_[index_of(size_)] = std::move(value);
+    ++size_;
+  }
+
+  template<typename... Args>
+  constexpr void emplace(Args&&... args)
+    requires std::is_constructible_v<value_type, Args...> {
+    assert(!full());
+    std::construct_at(&data_[index_of(size_)], std::forward<Args>(args)...);
+    ++size_;
+  }
+
+  [[nodiscard]] constexpr bool try_push(value_type const& value)
+    requires std::is_copy_assignable_v<value_type> {
+    if (full()) return false;
+    push(value);
+    return true;
+  }
+
+  [[nodiscard]] constexpr bool try_push(value_type&& value)
+    requires std::is_move_assignable_v<value_type> {
+    if (full()) return false;
+    push(std::move(value));
+    return true;
   }
 
   constexpr void pop() {
@@ -80,9 +117,199 @@ public:
 
   [[nodiscard]] constexpr std::optional<value_type> try_pop() {
     if (empty()) return std::nullopt;
-    auto v = front();
+    std::optional<value_type> result{front()};
     pop();
-    return v;
+    return result;
+  }
+};
+
+template<typename T, size_t Capacity>
+  requires(!std::is_default_constructible_v<T>
+           || !std::is_trivially_destructible_v<T>)
+       && (Capacity > 0)
+class inplace_queue<T, Capacity> {
+public:
+  using value_type = T;
+  using size_type = size_t;
+  using reference = value_type&;
+  using const_reference = value_type const&;
+  using pointer = value_type*;
+  using const_pointer = value_type const*;
+
+private:
+  struct no_value_t {};
+  union slot {
+    no_value_t no_value;
+    value_type value;
+    constexpr ~slot() {}
+  };
+
+  std::array<slot, Capacity> data_{};
+  static constexpr size_type capacity_ = Capacity;
+  size_type front_ = 0;
+  size_type size_ = 0;
+
+  constexpr size_type index_of(size_type offset) const {
+    return (front_ + offset) % capacity_;
+  }
+
+  constexpr pointer slot_ptr(size_type i) {
+    return &data_[i].value;
+  }
+
+  constexpr const_pointer slot_ptr(size_type i) const {
+    return &data_[i].value;
+  }
+
+public:
+  constexpr inplace_queue() = default;
+
+  constexpr inplace_queue(inplace_queue const& other)
+    requires std::is_copy_constructible_v<T> {
+    for (size_type i = 0; i < other.size_; ++i) {
+      std::construct_at(slot_ptr(i), *other.slot_ptr(other.index_of(i)));
+      ++size_;
+    }
+  }
+
+  constexpr inplace_queue(
+      inplace_queue&& other
+  ) noexcept(std::is_nothrow_move_constructible_v<T>)
+    requires std::is_move_constructible_v<T> {
+    for (size_type i = 0; i < other.size_; ++i) {
+      std::construct_at(
+          slot_ptr(i),
+          std::move(*other.slot_ptr(other.index_of(i)))
+      );
+      ++size_;
+    }
+    other.clear();
+  }
+
+  constexpr inplace_queue& operator=(inplace_queue const& other)
+    requires std::is_copy_constructible_v<T> {
+    if (this != &other) {
+      clear();
+      for (size_type i = 0; i < other.size_; ++i) {
+        std::construct_at(slot_ptr(i), *other.slot_ptr(other.index_of(i)));
+        ++size_;
+      }
+    }
+    return *this;
+  }
+
+  constexpr inplace_queue& operator=(
+      inplace_queue&& other
+  ) noexcept(std::is_nothrow_move_constructible_v<T>)
+    requires std::is_move_constructible_v<T> {
+    if (this != &other) {
+      clear();
+      for (size_type i = 0; i < other.size_; ++i) {
+        std::construct_at(
+            slot_ptr(i),
+            std::move(*other.slot_ptr(other.index_of(i)))
+        );
+        ++size_;
+      }
+      other.clear();
+    }
+    return *this;
+  }
+
+  constexpr ~inplace_queue() {
+    clear();
+  }
+
+  constexpr void clear() {
+    for (size_type i = 0; i < size_; ++i) {
+      std::destroy_at(slot_ptr(index_of(i)));
+    }
+    front_ = 0;
+    size_ = 0;
+  }
+
+  [[nodiscard]] constexpr bool empty() const {
+    return size_ == 0;
+  }
+
+  [[nodiscard]] constexpr bool full() const {
+    return size_ == capacity_;
+  }
+
+  [[nodiscard]] constexpr size_type capacity() const {
+    return capacity_;
+  }
+
+  [[nodiscard]] constexpr size_type size() const {
+    return size_;
+  }
+
+  [[nodiscard]] constexpr const_reference front() const {
+    assert(!empty());
+    return *slot_ptr(front_);
+  }
+
+  [[nodiscard]] constexpr const_reference back() const {
+    assert(!empty());
+    return *slot_ptr(index_of(size_ - 1));
+  }
+
+  constexpr void push(value_type const& value)
+    requires std::is_copy_constructible_v<value_type> {
+    assert(!full());
+    std::construct_at(slot_ptr(index_of(size_)), value);
+    ++size_;
+  }
+
+  constexpr void push(value_type&& value)
+    requires std::is_move_constructible_v<T> {
+    assert(!full());
+    std::construct_at(slot_ptr(index_of(size_)), std::move(value));
+    ++size_;
+  }
+
+  template<typename... Args>
+  constexpr void emplace(Args&&... args)
+    requires std::is_constructible_v<T, Args...> {
+    assert(!full());
+    std::construct_at(slot_ptr(index_of(size_)), std::forward<Args>(args)...);
+    ++size_;
+  }
+
+  [[nodiscard]] constexpr bool try_push(value_type const& value)
+    requires std::is_copy_constructible_v<value_type> {
+    if (full()) return false;
+    push(value);
+    return true;
+  }
+
+  [[nodiscard]] constexpr bool try_push(value_type&& value)
+    requires std::is_move_constructible_v<T> {
+    if (full()) return false;
+    push(std::move(value));
+    return true;
+  }
+
+  constexpr void pop() {
+    assert(!empty());
+    std::destroy_at(slot_ptr(front_));
+    front_ = (front_ + 1) % capacity_;
+    --size_;
+  }
+
+  [[nodiscard]] constexpr std::optional<value_type> try_pop()
+    requires std::is_move_constructible_v<T> || std::is_copy_constructible_v<T>
+  {
+    if (empty()) return std::nullopt;
+    if constexpr (std::is_move_constructible_v<T>) {
+      std::optional<value_type> result{std::move(*slot_ptr(front_))};
+      pop();
+      return result;
+    } else {
+      std::optional<value_type> result{*slot_ptr(front_)};
+      pop();
+      return result;
+    }
   }
 };
 
--- tests/inplace_queue_test.cpp
+++ tests/inplace_queue_test.cpp
@@ -89,6 +89,219 @@ static_assert(test_inplace_queue(emb::inplace_queue<int, 2>{}));
 static_assert(test_inplace_queue(emb::inplace_queue<int, 5>{}));
 static_assert(test_inplace_queue(emb::inplace_queue<int, 10>{}));
 
+struct tracked {
+  int value = 0;
+  int* counter = nullptr;
+
+  constexpr tracked() = default;
+  constexpr tracked(int v, int* c) : value(v), counter(c) {
+    if (counter) ++*counter;
+  }
+  constexpr tracked(tracked const& o) : value(o.value), counter(o.counter) {
+    if (counter) ++*counter;
+  }
+  constexpr tracked(tracked&& o) noexcept
+      : value(o.value), counter(o.counter) {
+    o.counter = nullptr;
+  }
+  constexpr tracked& operator=(tracked const& o) {
+    if (this != &o) {
+      if (counter) --*counter;
+      value = o.value;
+      counter = o.counter;
+      if (counter) ++*counter;
+    }
+    return *this;
+  }
+  constexpr tracked& operator=(tracked&& o) noexcept {
+    if (this != &o) {
+      if (counter) --*counter;
+      value = o.value;
+      counter = o.counter;
+      o.counter = nullptr;
+    }
+    return *this;
+  }
+  constexpr ~tracked() {
+    if (counter) --*counter;
+  }
+};
+
+static_assert(!std::is_trivially_destructible_v<tracked>);
+
+constexpr bool test_inplace_queue_lifecycle() {
+  int alive = 0;
+  {
+    emb::inplace_queue<tracked, 4> q;
+    assert(q.empty() && alive == 0);
+
+    q.emplace(1, &alive);
+    q.emplace(2, &alive);
+    assert(q.size() == 2 && alive == 2);
+    assert(q.front().value == 1);
+    assert(q.back().value == 2);
+
+    q.pop();
+    assert(q.size() == 1 && alive == 1);
+    assert(q.front().value == 2);
+
+    q.clear();
+    assert(q.empty() && alive == 0);
+
+    for (int i = 1; i <= 4; ++i) {
+      q.emplace(i, &alive);
+    }
+    assert(q.full() && alive == 4);
+
+    q.pop();
+    q.emplace(5, &alive);
+    assert(q.size() == 4 && alive == 4);
+    assert(q.front().value == 2);
+    assert(q.back().value == 5);
+  }
+  assert(alive == 0);
+  return true;
+}
+
+constexpr bool test_inplace_queue_copy() {
+  int alive = 0;
+  emb::inplace_queue<tracked, 4> q;
+  q.emplace(1, &alive);
+  q.emplace(2, &alive);
+  q.emplace(3, &alive);
+  assert(alive == 3);
+
+  {
+    auto copy = q;
+    assert(copy.size() == 3 && alive == 6);
+    assert(copy.front().value == 1);
+    assert(copy.back().value == 3);
+  }
+  assert(alive == 3);
+
+  emb::inplace_queue<tracked, 4> other;
+  other.emplace(100, &alive);
+  assert(alive == 4);
+
+  other = q;
+  assert(other.size() == 3 && alive == 6);
+  assert(other.front().value == 1);
+  assert(other.back().value == 3);
+
+  return true;
+}
+
+constexpr bool test_inplace_queue_move_push() {
+  int alive = 0;
+  emb::inplace_queue<tracked, 2> q;
+
+  tracked t{42, &alive};
+  assert(alive == 1);
+
+  q.push(std::move(t));
+  assert(alive == 1);
+  assert(q.front().value == 42);
+
+  return true;
+}
+
+constexpr bool test_inplace_queue_try_pop() {
+  int alive = 0;
+  emb::inplace_queue<tracked, 2> q;
+
+  auto empty_pop = q.try_pop();
+  assert(!empty_pop.has_value());
+
+  q.emplace(7, &alive);
+  assert(alive == 1);
+
+  auto popped = q.try_pop();
+  assert(popped.has_value());
+  assert(popped->value == 7);
+  assert(q.empty());
+  assert(alive == 1);
+
+  return true;
+}
+
+struct no_default {
+  int value;
+  constexpr no_default(int v) : value(v) {}
+};
+
+static_assert(!std::is_default_constructible_v<no_default>);
+
+constexpr bool test_inplace_queue_no_default_ctor() {
+  emb::inplace_queue<no_default, 3> q;
+  q.emplace(10);
+  q.emplace(20);
+  q.emplace(30);
+  assert(q.full());
+  assert(q.front().value == 10);
+  assert(q.back().value == 30);
+
+  q.pop();
+  assert(q.front().value == 20);
+
+  return true;
+}
+
+constexpr bool test_inplace_queue_move() {
+  int alive = 0;
+  emb::inplace_queue<tracked, 4> src;
+  src.emplace(1, &alive);
+  src.emplace(2, &alive);
+  src.emplace(3, &alive);
+  assert(alive == 3);
+
+  auto dst = std::move(src);
+  assert(dst.size() == 3 && alive == 3);
+  assert(dst.front().value == 1);
+  assert(dst.back().value == 3);
+  assert(src.empty());
+
+  emb::inplace_queue<tracked, 4> other;
+  other.emplace(99, &alive);
+  assert(alive == 4);
+
+  other = std::move(dst);
+  assert(other.size() == 3 && alive == 3);
+  assert(other.front().value == 1);
+  assert(other.back().value == 3);
+  assert(dst.empty());
+
+  return true;
+}
+
+constexpr bool test_inplace_queue_move_wraparound() {
+  int alive = 0;
+  emb::inplace_queue<tracked, 4> src;
+  for (int i = 1; i <= 4; ++i) {
+    src.emplace(i, &alive);
+  }
+  src.pop();
+  src.emplace(5, &alive);
+  assert(src.front().value == 2);
+  assert(src.back().value == 5);
+  assert(alive == 4);
+
+  auto dst = std::move(src);
+  assert(dst.size() == 4 && alive == 4);
+  assert(dst.front().value == 2);
+  assert(dst.back().value == 5);
+  assert(src.empty());
+
+  return true;
+}
+
+static_assert(test_inplace_queue_lifecycle());
+static_assert(test_inplace_queue_copy());
+static_assert(test_inplace_queue_move_push());
+static_assert(test_inplace_queue_try_pop());
+static_assert(test_inplace_queue_no_default_ctor());
+static_assert(test_inplace_queue_move());
+static_assert(test_inplace_queue_move_wraparound());
+
 } // namespace tests
 } // namespace internal
 } // namespace emb
