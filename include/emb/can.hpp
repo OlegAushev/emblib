2e6fcf28dc0db80df875a6c166b80c1b43f4aa2e
Oleg Aushev <aushevom@gmail.com>
2026-05-01 00:50:20 +0300

    update can types

--- include/emb/can.hpp
+++ include/emb/can.hpp
@@ -16,9 +16,11 @@ namespace emb {
 
 using canpayload_t = std::array<uint8_t, 8>;
 using canid_t = uint32_t;
+enum class canformat_t : uint8_t { standard, extended };
 
 struct canframe_t {
   canid_t id;
+  canformat_t format;
   uint8_t len;
   canpayload_t payload;
 };
