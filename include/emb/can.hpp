4832fc1f4c13fd3a634b0f81c0e8308a82acc816
Oleg Aushev <aushevom@gmail.com>
2026-05-14 16:24:58 +0300

    remove old can types

--- include/emb/can.hpp
+++ include/emb/can.hpp
@@ -3,15 +3,6 @@
 #include <array>
 #include <cstdint>
 
-using canpayload_t = std::array<uint8_t, 8>;
-using canid_t = uint32_t;
-
-struct can_frame {
-  canid_t id;
-  uint8_t len;
-  canpayload_t payload;
-};
-
 namespace emb {
 
 using canpayload_t = std::array<uint8_t, 8>;
@@ -19,8 +10,8 @@ using canid_t = uint32_t;
 enum class canformat_t : uint8_t { standard, extended };
 
 struct canframe_t {
-  canid_t id;
   canformat_t format;
+  canid_t id;
   uint8_t len;
   canpayload_t payload;
 };
--- tests/functional_test.cpp
+++ tests/functional_test.cpp
@@ -19,15 +19,15 @@ constexpr bool test_curry() {
 
   assert(curry(add3, 1, 2, 3) == 6);
 
-  auto f1 = curry(add3, 1);
+  [[maybe_unused]] auto f1 = curry(add3, 1);
   assert(f1(2, 3) == 6);
   assert(f1(10, 20) == 31);
 
-  auto f2 = curry(add3, 1, 2);
+  [[maybe_unused]] auto f2 = curry(add3, 1, 2);
   assert(f2(3) == 6);
   assert(f2(100) == 103);
 
-  auto f1_then_f2 = curry(add3, 1)(2);
+  [[maybe_unused]] auto f1_then_f2 = curry(add3, 1)(2);
   assert(f1_then_f2(3) == 6);
 
   assert(curry(add3, 1)(2)(3) == 6);
@@ -35,7 +35,7 @@ constexpr bool test_curry() {
   assert(curry(add3)(1, 2)(3) == 6);
   assert(curry(add3)(1)(2, 3) == 6);
 
-  auto lambda = [](int x, int y) { return x * y; };
+  [[maybe_unused]] auto lambda = [](int x, int y) { return x * y; };
   assert(curry(lambda, 3, 4) == 12);
   assert(curry(lambda)(3)(4) == 12);
 
